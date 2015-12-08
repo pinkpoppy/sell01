af0300762bd7c0ceed45cd85b5fa4e703592f83c
sszhu <nora.shasha.zhu@gmail.com>
2015-12-08 17:46:02 +0800

    add watch:all task

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,39 @@
 
 var gulp = require('gulp');
+
 var stylus = require('gulp-stylus');
 
-// var uglify = require('gulp-uglify');
-// var concat = require('gulp-concat');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify');
 
-// gulp.task('styles', function () {
-//   gulp.src('test.styl')
-//       .pipe(stylus())
-//       .pipe(gulp.dest('build/'));
-// });
+gulp.task('default',['watch:all']);
 
 gulp.task('styles', function () {
-  gulp.src('app/styl/main.styl')
-      .pipe(stylus())
+  gulp.src('app/main.styl')
+      .pipe(stylus({'include css':true}))
       .pipe(gulp.dest('build/'));
 });
 
+gulp.task('scripts',function(){
+  gulp.src(['app/*.js','vendor/*.js'])
+      .pipe(concat('main.js'))
+      .pipe(uglify())
+      .pipe(minify())
+      .pipe(gulp.dest('build/'));
+});
 
-// gulp.task('watch:styles',function(){
-// 	gulp.watch('**/*.styl',['styles']);
-// });
+gulp.task('watch:all',['styles','scripts'],function(){
+  gulp.watch(['app/js/*.js','vendor/*.js'],function(){
+    gulp.start('scripts');
+  });
+
+  gulp.watch(['app/plugin-css/*.css','app/styl/*.styl'],function(){
+    gulp.start('styles');
+  });
+});
+
+
+
 
 
