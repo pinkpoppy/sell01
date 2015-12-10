6acb8013acbfb4aa7a2f3e6d0893ea9740ac3d23
sszhu <nora.shasha.zhu@gmail.com>
2015-12-10 19:52:18 +0800

    update app/

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,14 @@
 
 var app = (function(){
   var configUrlMap = {
-    homeBannerAndNotify:'http://www.yaerku.com/pjms/tmBanner.php',
-    homeModule:'http://www.yaerku.com/pjms/tmHome.php'
+    //homeBannerAndNotify:'http://www.yaerku.com/pjms/tmBanner.php',
+    //homeModule:'http://www.yaerku.com/pjms/tmHome.php'
+    homeBannerAndNotify:'http://192.168.1.4:7784/tmBanner.php',
+    homeModule:'http://192.168.1.4:7784/tmHome.php'
+  }
+
+  function calScreenWidth(){
+    return screen.width
   }
   var loadDefaultData = function(path,des,ajaxCallback,data){
     $.ajax({
@@ -23,6 +29,9 @@ var app = (function(){
   }
   return {
     urls:configUrlMap,
-    get:loadDefaultData
-  };
+    get:loadDefaultData,
+    screenSize:calScreenWidth
+  }
+
+  // 432 / 720 = 0.6
 })();
