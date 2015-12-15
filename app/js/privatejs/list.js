d3d532cd6a07548532493ff9887b839fd04349fc
sszhu <nora.shasha.zhu@gmail.com>
2015-12-15 18:38:09 +0800

    fix createDocumentFragment bug

--- app/js/privatejs/list.js
+++ app/js/privatejs/list.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function($) {
   var arg = app.appState
 
+  var $listWrap = $('.wine-list-ul')
   function callBack(wineArr) {
     for (var i = 0; i < wineArr.length; i++) {
       console.log(wineArr.length)
-      app.methods.produceSeperateWineHtml(app,wineArr[i],$('.wine-list-ul'),i,wineArr.length)
+      app.methods.produceSeperateWineHtml(wineArr[i],$listWrap,i,wineArr.length)
     }
   }
   app.acquire(app.urls.list,'GET','获取指定 moduleID 的列表',arg,callBack)
