3de6b929fe97e81792367f48bc152333c3e8e427
sszhu <nora.shasha.zhu@gmail.com>
2016-01-13 19:18:53 +0800

    test

---
+++ rec.php
@@ -0,0 +1,14 @@
+<?php
+  echo($_SERVER['HTTP_USER_AGENT']);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>页面出错了...</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0 maximum-scale=-1.0,user-scalable=no">
+</head>
+<body>
+  <div class="wrap" style="font-size:1.2em;text-align:center;">当前仅支持在微信客户端打开!</div>
+</body>
+</html>
