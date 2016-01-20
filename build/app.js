39adf981fe12429d0a2899ebd3a3ef247e6b10f1
sszhu <nora.shasha.zhu@gmail.com>
2016-01-20 20:22:24 +0800

    u

--- build/app.js
+++ build/app.js
@@ -55,32 +55,6 @@ var app = (function(){
       storageArr:localArr
     };
 
-  alert("x")
-  
-  return {
-    urls:configUrlMap,
-    screenSize:calScreenWidth,
-    appState:appState,
-    config:config,
-    methods:{
-      produceSeperateWineHtml : produceSeperateWineHtml,
-      getSearchArgFromUrl :     getSearchArgFromUrl,
-      deviceInfo : deviceInfo,
-      appVersion : appVersion,
-      timestamp : timestamp,
-      getBasicUserinfo : getUserinfoData,
-      appAjax : appAjax,
-      browser : browserInfo,
-      pathInfo : pathInfo,
-      setBgImage :setBgImage,
-      initStorage,initStorage,
-      localArr:localArr,
-      setModalMask:setModalMask,
-      isLegalMobilePhone : isLegalMobilePhone
-    }
-  }
-
-
   function calScreenWidth(){
     return screen.width
   }
@@ -107,8 +81,8 @@ var app = (function(){
   
   function deviceInfo(){
     var 
-      devicePlat = browser.versions
-      device = ""
+      devicePlat = browser.versions,
+      device = "";
     if (devicePlat.ios) {
        device = "iphone"
     } else if (devicePlat.android) {
@@ -118,9 +92,7 @@ var app = (function(){
   }  
 
   function browserInfo(){
-    alert("ENTER BROWSER INFO")
     if (browser.versions.mobile) {
-      alert("MOBILE")
       var ua = navigator.userAgent.toLowerCase()
       if (ua.match(/MicroMessenger/i) == "micromessenger") {
         return "weixin"
@@ -153,11 +125,11 @@ var app = (function(){
 
   function pathInfo(addPath){
     var
-      l = window.location
-      host = l.protocol + "//" + l.host
-      pathname = l.pathname
-      path = pathname.substring(0,pathname.lastIndexOf('/')+1)
-      newPathName = host + path + addPath
+      l = window.location,
+      host = l.protocol + "//" + l.host,
+      pathname = l.pathname,
+      path = pathname.substring(0,pathname.lastIndexOf('/')+1),
+      newPathName = host + path + addPath;
 
     return newPathName
   }
@@ -165,7 +137,7 @@ var app = (function(){
   function initStorage(type) {
     try {
       var 
-        storage = window[type]
+        storage = window[type];
       return true
     }
     catch(e) {
@@ -176,10 +148,10 @@ var app = (function(){
   function AES(plainText,timestamp) {
 
     pkcs7 = function(str) {
-      var len = str.length
-          block_size = 32
-          pad = block_size-(len % block_size)
-          padChar=String.fromCharCode(pad)
+      var len = str.length,
+          block_size = 32,
+          pad = block_size-(len % block_size),
+          padChar=String.fromCharCode(pad);
 
       for (var i = 0; i < pad; i++) {
         str = str + padChar
@@ -187,25 +159,28 @@ var app = (function(){
       return str
     }
 
-    var Base64Key = config['Base64Key'] + timestamp + '='
-        key = CryptoJS.enc.Base64.parse(Base64Key)
-        iv = key.left(16)
+    var Base64Key = config['Base64Key'] + timestamp + '=',
+        key = CryptoJS.enc.Base64.parse(Base64Key),
+        iv = key.left(16);
 
-    //var text = pkcs7(unescape(encodeURIComponent(plainText)))
-    //alert(text)
-
-    //text=CryptoJS.enc.Latin1.parse(plainText);
-    var text=CryptoJS.enc.Latin1.parse(pkcs7(plainText))
+    plainText = unescape(encodeURIComponent(plainText))
+    var text=CryptoJS.enc.Latin1.parse(pkcs7(plainText));
         ciphertext = CryptoJS.AES.encrypt(text, 
                                           key, 
                                           {iv: iv, 
                                             mode:CryptoJS.mode.CBC, 
                                             padding:CryptoJS.pad.NoPadding}),
         base64Text = CryptoJS.enc.Base64.stringify(ciphertext.ciphertext)
-        console.log("base64Text =" + base64Text)
     return base64Text
   }
 
+  /**
+  *@param Des descripption of the request String
+  *@param MethodName specific API name of current request String
+  *@param RequestType GET/POST or Other Type String
+  *@param userData JSON
+  *@param Timestamp Current Timestamp an 10bit Interger
+  */
   function appAjax(des,
                    methodName,
                    requestType,
@@ -214,8 +189,8 @@ var app = (function(){
                    succeedCallback
                    ) {
     var 
-      path = configUrlMap['APIBase']
-      data = jointPostData(methodName,timestamp,AES(userData,timestamp))
+      path = configUrlMap['APIBase'],
+      data = jointPostData(methodName,timestamp,AES(userData,timestamp));
     $.ajax({
       url : path,
       method : requestType,
@@ -224,18 +199,25 @@ var app = (function(){
     })
     .done(function(data) {
       ajaxLog(path,des,'successed')
+      //var json = JSON.parse(JSON.stringify(eval( "(" + data +")")))
+      //callBack(json)
       succeedCallback(data)
     })
     .fail(function(data) {
+      //failedCallback(data)
       ajaxLog(path,des,'failed')
     })
     .always(function(data) {
+      //alwaysCallback(data)
       ajaxLog(path,des,'completed')
     });
   }
 
+  /**
+  *   
+  */
   function getUserinfoData(){
-    var u = {}
+    var u = {};
     u.userId = config.userId
     u.userType = config.userType
     u.headPic = config.headPic
@@ -248,10 +230,38 @@ var app = (function(){
     return u
   }
 
-
+  /**
+  * @m API method name
+  * @t timestamp using for AES
+  * @p base64text representing the sensitive user info
+  */
   function jointPostData(m,t,p) {
     return data = {"m" : m,"t" : t,"p" : p}
   }
+
+  return {
+    urls:configUrlMap,
+    screenSize:calScreenWidth,
+    appState:appState,
+    config:config,
+    methods:{
+      produceSeperateWineHtml : produceSeperateWineHtml,
+      getSearchArgFromUrl :     getSearchArgFromUrl,
+      deviceInfo : deviceInfo,
+      appVersion : appVersion,
+      timestamp : timestamp,
+      getBasicUserinfo : getUserinfoData,
+      appAjax : appAjax,
+      browser : browserInfo,
+      pathInfo : pathInfo,
+      setBgImage :setBgImage,
+      initStorage,initStorage,
+      localArr:localArr,
+      setModalMask:setModalMask,
+      isLegalMobilePhone : isLegalMobilePhone
+    }
+  }
+
   function setBgImage(str) {
     if (str.search('focus') != -1) {//点击的时候是选中状态
       str = str.replace('focus','blur')
@@ -263,7 +273,7 @@ var app = (function(){
   
   function produceSeperateWineHtml(currentWine,wrap,wineIndex,moduleIndex,moduleLength,wineLength,moduleId) 
   {
-    var itemHeight = 0
+    var itemHeight = 0;
     //console.log("arguments.length = " + arguments.length)
     if (arguments.length==5) {
       currentWine = arguments[0],
@@ -273,28 +283,27 @@ var app = (function(){
       moduleId = arguments[4];
     }
     var 
-      screenWidth = app.screenSize()
-      //imgWidth = liWidth = screenWidth * 0.425
-      imgWidth = liWidth = parseInt(parseInt((screenWidth - 26) / 2))
-      ratio = 1.0 //图片原始的高宽比:432 / 720 = 0.6
-      imgHeight = Math.ceil(imgWidth * ratio)
-      liBottomHeight = 70
-      liHeight = imgHeight + liBottomHeight
+      screenWidth = app.screenSize(),
+      imgWidth = liWidth = parseInt(parseInt((screenWidth - 26) / 2)),
+      ratio = 1.0, //图片原始的高宽比:432 / 720 = 0.6
+      imgHeight = Math.ceil(imgWidth * ratio),
+      liBottomHeight = 70,
+      liHeight = imgHeight + liBottomHeight,
 
-    $li = $("<li></li>")
-    $wineWrap = $("<div class='wine-wrap'></div>") //包住酒款
-    $divUp = $("<div class='div-up'></div>") //酒款上部
-    $divBottom = $("<div class='div-bottom'></div>") //酒款下部
+    $li = $("<li></li>"),
+    $wineWrap = $("<div class='wine-wrap'></div>"), //包住酒款
+    $divUp = $("<div class='div-up'></div>"), //酒款上部
+    $divBottom = $("<div class='div-bottom'></div>"), //酒款下部
 
     $awinePic = $("<a class='wine-detail' href='"
                     +pathInfo("detail.php?id=")
                     +currentWine['id']
                     +"'data-id='"
                     +currentWine['id']
-                    +"'></a>")
+                    +"'></a>"),
 
-    $spanMailInfo = $("<span class='mail-info'>"+currentWine['discount']+"</span>") //满200包邮 => 酒款上部
-    $spanCntInfo = $("<span class='cnt-info'>"+currentWine['shortage']+"</span>") //仅剩6 => 酒款上部
+    $spanMailInfo = $("<span class='mail-info'>"+currentWine['discount']+"</span>"), //满200包邮 => 酒款上部
+    $spanCntInfo = $("<span class='cnt-info'>"+currentWine['shortage']+"</span>"); //仅剩6 => 酒款上部
     
     if (currentWine['pics'].length!=0) {
       $img = $("<img class='wine-img' src='"+currentWine['pics'][0]['pic']+"'>") //酒款图片 => 酒款上部
@@ -307,40 +316,41 @@ var app = (function(){
       height:imgHeight
     })
 
-    $spanSubtitle = $("<span class='subtitle'>"+currentWine['subtitle']+"</span>") //副标题 => 酒款上部
+  var 
+    $spanSubtitle = $("<span class='subtitle'>"+currentWine['subtitle']+"</span>"), //副标题 => 酒款上部
 
-    $divHeadline = $("<div>"+currentWine['title']+"</div>") //主描述 => 酒款下部
-    $spanCurrentPrice = $("<span class='market'>"+'&yen;'+currentWine['market']+"</span>")//现价 => 酒款下部
-    $spanOriginalPrice = $("<del>"+currentWine['price']+"</del>")//原价 or 其他标注 => 酒款下部
-    $spanRecom = $("<span>"+currentWine['limit']+"</span>")//主描述 => 提示
+    $divHeadline = $("<div>"+currentWine['title']+"</div>"), //主描述 => 酒款下部
+    $spanCurrentPrice = $("<span class='market'>"+'&yen;'+currentWine['market']+"</span>"),//现价 => 酒款下部
+    $spanOriginalPrice = $("<del>"+currentWine['price']+"</del>"),//原价 or 其他标注 => 酒款下部
+    $spanRecom = $("<span>"+currentWine['limit']+"</span>"),//主描述 => 提示
     
-    $awinePic.append($img)
-    $divUp.append($awinePic)
-    $divUp.append($spanMailInfo)
-    $divUp.append($spanCntInfo)
-    $divUp.append($spanSubtitle)
+    $awinePic.append($img),
+    $divUp.append($awinePic),
+    $divUp.append($spanMailInfo),
+    $divUp.append($spanCntInfo),
+    $divUp.append($spanSubtitle),
 
 
-    $divBottom.append($divHeadline)
-    $divBottom.append($spanCurrentPrice)
-    $divBottom.append($spanOriginalPrice)
-    $divBottom.append($spanRecom)
+    $divBottom.append($divHeadline),
+    $divBottom.append($spanCurrentPrice),
+    $divBottom.append($spanOriginalPrice),
+    $divBottom.append($spanRecom),
 
     // 设置 divUp 以及 divBottom 的高度
-    $divUp.css('height', imgHeight)
-    $divBottom.css('height', liBottomHeight)
-    $awinePic.css('height', imgHeight)
+    $divUp.css('height', imgHeight),
+    $divBottom.css('height', liBottomHeight),
+    $awinePic.css('height', imgHeight),
 
-    $wineWrap.append($divUp)
-    $wineWrap.append($divBottom)
-    $li.append($wineWrap)
+    $wineWrap.append($divUp),
+    $wineWrap.append($divBottom),
+    $li.append($wineWrap);
 
 
     var 
-      y = parseInt(wineIndex / 2) * liHeight
-      base = 42
-      liMargin = 15
-      rightColumnStart = 8 + liWidth + 10
+      y = parseInt(wineIndex / 2) * liHeight,
+      base = 42,
+      liMargin = 15,
+      rightColumnStart = 8 + liWidth + 10;
     if (wineIndex == 0) {
       $li.css({
         left: '8',
@@ -366,6 +376,7 @@ var app = (function(){
       
     }
 
+  
     $wineWrap.css('width', liWidth)
     
     $(wrap).append($li)
@@ -391,10 +402,10 @@ var app = (function(){
   //设置模态对话框遮罩层宽高
   function setModalMask(ele) {
     var 
-      screenW = parseFloat(calScreenWidth())
-      screenH = parseFloat(calScreenHeight())
-      boxW = Math.ceil(parseFloat(screenW * 0.84))
-      boxH = Math.ceil(parseFloat(screenH * 0.46))
+      screenW = parseFloat(calScreenWidth()),
+      screenH = parseFloat(calScreenHeight()),
+      boxW = Math.ceil(parseFloat(screenW * 0.84)),
+      boxH = Math.ceil(parseFloat(screenH * 0.46));
     $(ele).css('width',screenW+'px')
     $(ele).css('height',screenH+'px')
     $(ele).children('.modal-body').css('width',boxW + 'px')
@@ -410,7 +421,7 @@ var app = (function(){
 
   function isLegalMobilePhone(str) {
     var
-      mpReg = /1[^0126][0-9]{9}/
+      mpReg = /1[^0126][0-9]{9}/;
     if (isInputEmpty(str)) {
       return "EMPTY"
     } else {
